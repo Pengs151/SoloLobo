211ae9898e7ae4856528825cf21866eaa4f91376
Daymon Worldwide <daymonworldwide@C02K5964FFT0.local>
2018-11-26 17:14:09 -0800

    Updates

---
+++ precache-manifest.03d4e56d5987c6574f706dd23bc119e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a674cd512d246c2a39729c82d55fb09",
+    "url": "/Test/static/media/strength2.0a674cd5.jpeg"
+  },
+  {
+    "revision": "c9201d40606519ca2549c3482c9aaf7e",
+    "url": "/Test/static/media/Spyro.c9201d40.jpg"
+  },
+  {
+    "revision": "905a85fb29903b0c9e8fdc90e9f7299a",
+    "url": "/Test/static/media/03_NEW-TO-DnD_Races_InlineR_RacesGeneral.905a85fb.jpg"
+  },
+  {
+    "revision": "658d72dc480d2e8be55b",
+    "url": "/Test/static/js/runtime~main.658d72dc.js"
+  },
+  {
+    "revision": "9bfca2c2b6a3f46c29bd",
+    "url": "/Test/static/js/main.9bfca2c2.chunk.js"
+  },
+  {
+    "revision": "804808bef6beae3968d7",
+    "url": "/Test/static/js/1.804808be.chunk.js"
+  },
+  {
+    "revision": "9bfca2c2b6a3f46c29bd",
+    "url": "/Test/static/css/main.985e53ef.chunk.css"
+  },
+  {
+    "revision": "b482b5cd4d7f6e802639a40e68db337a",
+    "url": "/Test/index.html"
+  }
+];
